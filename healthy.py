ecb1f9a9a9a2a30c42b84077c6aac590ac2a5433
bytesys <92568399+bytesys@users.noreply.github.com>
2022-09-20 20:32:29 +0800

    Update healthy.py

--- healthy.py
+++ healthy.py
@@ -1,4 +1,9 @@
 import os
 
-print(os.environ["username"])
-print(os.environ["password"])
+if __name__ == "__main__":
+  username = os.environ["username"]
+  password = os.environ["password"]
+  if len(username) == 0 and len(password) == 0:
+    print("用户名或密码未设置！！！")
+    exit(0)
+  print("用户名与密码设置成功，正在开始执行后续代码...")
