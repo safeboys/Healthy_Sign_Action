9166e9073c0514b8d0fcfd002ec069f8de519d3b
bytesys <92568399+bytesys@users.noreply.github.com>
2022-09-21 19:02:05 +0800

    Update healthy.py

--- healthy.py
+++ healthy.py
@@ -1,5 +1,6 @@
 import os
 import requests
+from hashlib import md5
 
 if __name__ == "__main__":
   push = os.environ["push"]
@@ -11,3 +12,15 @@ if __name__ == "__main__":
   if len(push) == 0:
     print("未设置PUSH_KEY，无法使用Server酱推送运行日志！！！")
   print("用户名与密码设置成功，正在开始执行后续代码...")
+  
+  request = requests.session()
+  header = {
+    "User-Agent" : "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Mobile Safari/537.36"
+  }
+  body = {
+    "uname" : username,
+    "pd_mm" : md5(password.encode()).hexdigest()
+  }
+  url = 'http://xggl.hnqczy.com/website/login'
+  response = request.post(url,headers=header,data=body)
+  print(response.text)
