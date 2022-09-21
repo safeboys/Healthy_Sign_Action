6f1f4bca1b2a94461bbb5e64c78cc3425507a2bf
bytesys <92568399+bytesys@users.noreply.github.com>
2022-09-21 10:31:50 +0800

    Update healthy.py

--- healthy.py
+++ healthy.py
@@ -1,9 +1,13 @@
 import os
+import requests
 
 if __name__ == "__main__":
+  push = os.environ["push"]
   username = os.environ["username"]
   password = os.environ["password"]
   if len(username) == 0 and len(password) == 0:
     print("用户名或密码未设置！！！")
     exit(0)
+  if len(push) == 0:
+    print("未设置PUSH_KEY，无法使用Server酱推送运行日志！！！")
   print("用户名与密码设置成功，正在开始执行后续代码...")
